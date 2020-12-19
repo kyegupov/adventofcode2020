e26adfa73d661910136c3d5eb82f290ad154b45b
Konstantin Yegupov <kyegupov4@gmail.com>
2020-12-19 11:17:25 +0000

    Day 19

---
+++ day19/b.py
@@ -0,0 +1,50 @@
+import re
+
+input = open('input.txt').readlines()
+
+rules = {}
+mode = 'rx'
+res = 0
+
+def buildrx(n):
+    if n == '8':
+        return '('+buildrx('42')+')+'
+    if n == '11':
+        a = []
+        for i in range(1,20):
+            a.append(f'({buildrx("42")}{{{i}}}{buildrx("31")}{{{i}}})')
+        return '('+('|'.join(a))+')'
+
+
+    s = ""
+    has_bar = False
+    for part in rules[n]:
+        if part in rules:
+            s += buildrx(part)
+        elif part == "|":
+            s = '('+s+'|'
+            has_bar = True
+        elif part.startswith('"'):
+            s += part.strip('"')
+        else:
+            raise part
+    if has_bar:
+        s += ')'
+    return s
+
+
+for l in input:
+    if l.strip() == '':
+        mode = 'data'
+        rx0s = buildrx('0')
+        print(rx0s)
+        rx0 = re.compile("^"+rx0s+'$')
+    if mode == 'rx':
+        n, val = l.split(':')
+        rules[n] = val.strip().split(' ')
+    if mode == 'data':
+        # print(rx0, l.strip(), rx0.match(l.strip()))
+        if rx0.match(l.strip()):
+            res += 1
+
+print(res)
